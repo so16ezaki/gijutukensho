f3ace74298641d6410e96ebfdfc60b252bedd4b3
江崎　壮一郎 <2024_s-ezaki_alsi@techseed.jp>
2024-05-16 08:46:44 +0900

    センサ範囲可変

--- Assets/Scripts/Sensor.cs
+++ Assets/Scripts/Sensor.cs
@@ -6,45 +6,63 @@ public class Sensor : MonoBehaviour
 {
     [SerializeField] Text text;
     [SerializeField] Image image;
-
+    [SerializeField] GameObject obj;
     [SerializeField] Texture2D tex;
+    [SerializeField] int areaSize = 30; // Adjust as needed
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
-        int maxDistance = 2;
+        float maxDistance = 1f;
 
-        Ray ray = new Ray(transform.position,Vector3.down);
-        Debug.DrawRay(transform.position, Vector3.down * maxDistance, Color.red);
+        Ray ray = new Ray(transform.position, -transform.up * maxDistance);
+        Debug.DrawRay(transform.position, -transform.up, Color.red);
 
         RaycastHit hit;
-        
+
+
         // CubeÇÃíÜêSÇ©ÇÁâ∫ï˚å¸Ç÷rayÇêLÇŒÇ∑
-        if (Physics.Raycast(transform.position, Vector3.down, out hit,maxDistance))
+        if (Physics.Raycast(ray, out hit, maxDistance))
         {
+
+            obj = hit.collider.gameObject;
             tex = hit.collider.gameObject.GetComponent<MeshRenderer>().material.mainTexture as Texture2D;
             Vector2 uv = hit.textureCoord;
-            Color[] pix = tex.GetPixels(Mathf.FloorToInt(uv.x * tex.width), Mathf.FloorToInt(uv.y * tex.height), 1, 1);
-            //Debug.Log(pix[0].ToString());
-            //Debug.Log(pix[0]);
-            Color a = pix[0];
 
-            float h, s, v;
-            Color.RGBToHSV(a, out h, out s, out v);
-            //Debug.Log("h" + h + "s" + s + "v" + v);
-            Debug.Log("ñæìx:" + v);
+            
+
+            // Calculate the average brightness
+            float totalBrightness = 0f;
+            int pixelCount = 0;
+
+            for (int x = -areaSize / 2; x <= areaSize / 2; x++)
+            {
+                for (int y = -areaSize / 2; y <= areaSize / 2; y++)
+                {
+                    Color[] pixels = tex.GetPixels(Mathf.FloorToInt(uv.x * tex.width) + x, Mathf.FloorToInt(uv.y * tex.height) + y, 1, 1);
+                    Color pixelColor = pixels[0];
+                    float brightness;
+                    Color.RGBToHSV(pixelColor, out _, out _, out brightness);
+                    totalBrightness += brightness;
+                    pixelCount++;
+                }
+            }
+
+            float averageBrightness = totalBrightness / pixelCount;
+            Debug.Log("Average Brightness: " + averageBrightness);
+
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-       
 
-        
+
+
 
     }
 }
